39522864b60000d38e83be50a7ed802b4f014478
Josep Albert Tortosa Peris <joseptortosa33@gmail.com>
2023-05-17 21:15:03 +0200

    Verificació de registre
    Falta, una vegada verificar els camps, crear obj usuari i afegir la info a la BD. Després s'haurà d'implementar la verificació en l'inici de sessió

--- projecteprofinal/src/main/java/com/grup/projecteprofinal/InterficieRegistre.java
+++ projecteprofinal/src/main/java/com/grup/projecteprofinal/InterficieRegistre.java
@@ -1,29 +1,35 @@
 package com.grup.projecteprofinal;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
-import java.awt.GridBagLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class InterficieRegistre extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
+	private JTextField textNom;
+	private JTextField textCognoms;
+	private JTextField textPoblacio;
+	private JTextField textCorreu;
+	private JPasswordField passwordText1;
+	private JPasswordField passwordText2;
+	private JComboBox<String> comboBox = new JComboBox<String>();
+	private String[] componentsCadena = new String[7];
 
 	/**
 	 * Launch the application.
@@ -45,6 +51,7 @@ public class InterficieRegistre extends JFrame {
 	 * Create the frame.
 	 */
 	public InterficieRegistre() {
+		setTitle("Registre");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -57,9 +64,11 @@ public class InterficieRegistre extends JFrame {
 		contentPane.add(panel, BorderLayout.SOUTH);
 
 		JButton btnNewButton = new JButton("Registrar-se");
+
 		panel.add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("Cancel·la");
+
 		panel.add(btnNewButton_1);
 
 		JPanel panel_1 = new JPanel();
@@ -87,14 +96,14 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_1.gridy = 1;
 		panel_1.add(nom, gbc_lblNewLabel_1);
 
-		textField = new JTextField();
+		textNom = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 5;
 		gbc_textField.gridy = 1;
-		panel_1.add(textField, gbc_textField);
-		textField.setColumns(10);
+		panel_1.add(textNom, gbc_textField);
+		textNom.setColumns(10);
 
 		JLabel cognoms = new JLabel("Cognoms:");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
@@ -105,14 +114,14 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_2.gridy = 2;
 		panel_1.add(cognoms, gbc_lblNewLabel_2);
 
-		textField_1 = new JTextField();
+		textCognoms = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 5;
 		gbc_textField_1.gridy = 2;
-		panel_1.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
+		panel_1.add(textCognoms, gbc_textField_1);
+		textCognoms.setColumns(10);
 
 		JLabel poblacio = new JLabel("Població:");
 		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
@@ -123,14 +132,14 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_4.gridy = 3;
 		panel_1.add(poblacio, gbc_lblNewLabel_4);
 
-		textField_2 = new JTextField();
+		textPoblacio = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_2.gridx = 5;
 		gbc_textField_2.gridy = 3;
-		panel_1.add(textField_2, gbc_textField_2);
-		textField_2.setColumns(10);
+		panel_1.add(textPoblacio, gbc_textField_2);
+		textPoblacio.setColumns(10);
 
 		JLabel correu = new JLabel("Correu:");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
@@ -141,14 +150,14 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_3.gridy = 4;
 		panel_1.add(correu, gbc_lblNewLabel_3);
 
-		textField_3 = new JTextField();
+		textCorreu = new JTextField();
 		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
 		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_3.gridx = 5;
 		gbc_textField_3.gridy = 4;
-		panel_1.add(textField_3, gbc_textField_3);
-		textField_3.setColumns(10);
+		panel_1.add(textCorreu, gbc_textField_3);
+		textCorreu.setColumns(10);
 
 		JLabel contra1 = new JLabel("Contrassenya: ");
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
@@ -159,13 +168,13 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_5.gridy = 5;
 		panel_1.add(contra1, gbc_lblNewLabel_5);
 
-		passwordField = new JPasswordField();
+		passwordText1 = new JPasswordField();
 		GridBagConstraints gbc_passwordField = new GridBagConstraints();
 		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_passwordField.gridx = 5;
 		gbc_passwordField.gridy = 5;
-		panel_1.add(passwordField, gbc_passwordField);
+		panel_1.add(passwordText1, gbc_passwordField);
 
 		JLabel contra2 = new JLabel("Repeteix la contrassenya:");
 		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
@@ -176,13 +185,13 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_6.gridy = 6;
 		panel_1.add(contra2, gbc_lblNewLabel_6);
 
-		passwordField_1 = new JPasswordField();
+		passwordText2 = new JPasswordField();
 		GridBagConstraints gbc_passwordField_1 = new GridBagConstraints();
 		gbc_passwordField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_passwordField_1.gridx = 5;
 		gbc_passwordField_1.gridy = 6;
-		panel_1.add(passwordField_1, gbc_passwordField_1);
+		panel_1.add(passwordText2, gbc_passwordField_1);
 
 		JLabel lblNewLabel_1 = new JLabel("Selecciona el teu avatar:");
 		GridBagConstraints gbc_lblNewLabel_11 = new GridBagConstraints();
@@ -192,7 +201,6 @@ public class InterficieRegistre extends JFrame {
 		gbc_lblNewLabel_11.gridy = 7;
 		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_11);
 
-		JComboBox<String> comboBox = new JComboBox<String>();
 		comboBox.addItem("Poma");
 		comboBox.addItem("Pera");
 		comboBox.addItem("Llima");
@@ -206,8 +214,44 @@ public class InterficieRegistre extends JFrame {
 		gbc_comboBox.gridx = 5;
 		gbc_comboBox.gridy = 7;
 		panel_1.add(comboBox, gbc_comboBox);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				componentsCadena[0] = textNom.getText();
+				componentsCadena[1] = textCognoms.getText();
+				componentsCadena[2] = textPoblacio.getText();
+				componentsCadena[3] = textCorreu.getText();
+				String pwd1 = new String(passwordText1.getPassword());
+				componentsCadena[4] = pwd1;
+				String pwd2 = new String(passwordText2.getPassword());
+				componentsCadena[5] = pwd2;
+				componentsCadena[6] = (String) comboBox.getSelectedItem();
+				boolean correcte = true;
+				for (String cadena : componentsCadena) {
+					if (cadena == null || cadena.isBlank() || cadena.isEmpty()) {
+						correcte = false;
+						break;
+
+					}
+				}
+				if (correcte == true) {
+					ProcessamentRegistre.procesamentCampsRegistre(componentsCadena[0], componentsCadena[1],
+							componentsCadena[2], componentsCadena[3], componentsCadena[4], componentsCadena[5],
+							componentsCadena[6]);
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Cal plenar tots els camps!", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 
 		setVisible(true);
+
 	}
 
 }
--- projecteprofinal/src/main/java/com/grup/projecteprofinal/ProcessamentRegistre.java
+++ projecteprofinal/src/main/java/com/grup/projecteprofinal/ProcessamentRegistre.java
@@ -1,9 +1,40 @@
 package com.grup.projecteprofinal;
 
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
+
 public class ProcessamentRegistre {
 
 	public ProcessamentRegistre() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public static void procesamentCampsRegistre(String nom, String cognoms, String poblacio, String correu,
+			String password1, String password2, String imatge) {
+		//Paràmetres a comprovar correu i correspondència entre contrassenyes.
+		//també que les contrassenyes tinguen com a mínim 8 caràcters.
+		 String regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+		 boolean correuCorrecte = Pattern.compile(regex).matcher(correu).matches();
+		 boolean longitudCorrecta = (password1.length()>=8) && (password2.length()>=8);
+		 boolean sonIguals = password1.equals(password2);
+		 if (!correuCorrecte) {
+		        JOptionPane.showMessageDialog(null, "El format de correu introduït no és vàlid", "Error", JOptionPane.ERROR_MESSAGE);
+		        return;
+		    }
+
+		    if (!longitudCorrecta) {
+		        JOptionPane.showMessageDialog(null, "Les contrassenyes deuen tindre almenys 8 caràcters", "Error", JOptionPane.ERROR_MESSAGE);
+		        return;
+		    }
+
+		    if (!sonIguals) {
+		        JOptionPane.showMessageDialog(null, "Les contrassenyes no coincideixen", "Error", JOptionPane.ERROR_MESSAGE);
+		        return;
+		    }
+
+		 
+
 	}
 
 }
