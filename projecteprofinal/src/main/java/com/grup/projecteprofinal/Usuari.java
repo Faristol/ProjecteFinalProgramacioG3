bf57c435f0f74b95dd0b0104223b6f7f7c8b35ec
Josep Albert Tortosa Peris <joseptortosa33@gmail.com>
2023-05-17 19:14:30 +0200

    Reconfiguració projecte, inicialització del mateix

---
+++ projecteprofinal/src/main/java/com/grup/projecteprofinal/Usuari.java
@@ -0,0 +1,15 @@
+package com.grup.projecteprofinal;
+
+public class Usuari {
+	private String nom = null;
+	private String cognoms = null;
+	private String poblacio = null;
+	private String correuElectronic=null;
+	private String imatge = "imatgeDefecte.jpg";
+	//private int fortalesa = 
+
+	public Usuari() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
